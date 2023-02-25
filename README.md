e4351ef3cdc735dc1173b8f5f5ce533363cd77ec
Andrej Marinchenko <andrej.marinchenko@gmail.com>
2023-02-25 21:34:56 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+<p>
+  <img  src="https://img.shields.io/github/stars/BEPb/Kaggle_Stable_Diffusion_Image_to_Prompts" />
+  <img src="https://img.shields.io/github/contributors/BEPb/Kaggle_Stable_Diffusion_Image_to_Prompts" />
+  <img src="https://img.shields.io/github/last-commit/BEPb/Kaggle_Stable_Diffusion_Image_to_Prompts" />
+  <img src="https://visitor-badge.laobi.icu/badge?page_id=BEPb.Kaggle_Stable_Diffusion_Image_to_Prompts" />
+  <img src="https://img.shields.io/github/languages/count/BEPb/Kaggle_Stable_Diffusion_Image_to_Prompts" />
+  <img src="https://img.shields.io/github/languages/top/BEPb/Kaggle_Stable_Diffusion_Image_to_Prompts" />
+  <img src="https://img.shields.io/badge/license-MIT-blue.svg?color=f64152" />
+  <img  src="https://img.shields.io/github/issues/BEPb/Kaggle_Stable_Diffusion_Image_to_Prompts" />
+  <img  src="https://img.shields.io/github/issues-pr/BEPb/Kaggle_Stable_Diffusion_Image_to_Prompts" />
+</p>
+<div align="center">
+
+<img src="./art/logo.png" alt="Bot logo" width="300" height="156.5">
+
+# kaggle Stable Diffusion - Image to Prompts
+
+</div>
+
+## How it works?
+
+It's very simple: here are the solutions for the [Stable Diffusion - Image to Prompts](https://www.kaggle.com/competitions/stable-diffusion-image-to-prompts/overview)
+
+## Order of preparation and work
+
+1. Clone the repository or download the archive from github or using the following commands on the command line
+    ```command line
+    $cmd
+    $ git clone https://github.com/BEPb/Kaggle_Stable_Diffusion_Image_to_Prompts
+    $ cd Kaggle_Stable_Diffusion_Image_to_Prompts
+    ```
+
+2. Create a Python virtual environment.
+3. Install all necessary packages for our code to work using the following command:
+
+     ```
+     pip install -r requirements.txt
+     ```
+
+Check GPU
+версия видеокарты вашего ПК
+```commandline
+ nvidia-smi -L
+```
+
+
---
+++ main.py
@@ -0,0 +1,95 @@
+"""
+Python 3.10 программа для распознования изображения и написания краткого комментария нарисованного
+Название 00.main.py
+
+Version: 0.1
+Author: Andrej Marinchenko
+Date: 2023-02-25
+"""
+# import subprocess
+#
+# def setup():
+#     install_cmds = [
+#         ['pip', 'install', 'gradio'],
+#         ['pip', 'install', 'open_clip_torch'],
+#         ['pip', 'install', 'clip-interrogator'],
+#     ]
+#     for cmd in install_cmds:
+#         print(subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode('utf-8'))
+
+# setup()
+
+clip_model_name = 'ViT-L-14/openai'  # @param ["ViT-L-14/openai", "ViT-H-14/laion2b_s32b_b79k"]
+
+import gradio as gr
+from clip_interrogator import Config, Interrogator
+
+config = Config()
+config.blip_num_beams = 64
+config.blip_offload = False
+config.clip_model_name = clip_model_name
+ci = Interrogator(config)
+
+def image_analysis(image):
+    image = image.convert('RGB')
+    image_features = ci.image_to_features(image)
+
+    top_mediums = ci.mediums.rank(image_features, 5)
+    top_artists = ci.artists.rank(image_features, 5)
+    top_movements = ci.movements.rank(image_features, 5)
+    top_trendings = ci.trendings.rank(image_features, 5)
+    top_flavors = ci.flavors.rank(image_features, 5)
+
+    medium_ranks = {medium: sim for medium, sim in zip(top_mediums, ci.similarities(image_features, top_mediums))}
+    artist_ranks = {artist: sim for artist, sim in zip(top_artists, ci.similarities(image_features, top_artists))}
+    movement_ranks = {movement: sim for movement, sim in
+                      zip(top_movements, ci.similarities(image_features, top_movements))}
+    trending_ranks = {trending: sim for trending, sim in
+                      zip(top_trendings, ci.similarities(image_features, top_trendings))}
+    flavor_ranks = {flavor: sim for flavor, sim in zip(top_flavors, ci.similarities(image_features, top_flavors))}
+
+    return medium_ranks, artist_ranks, movement_ranks, trending_ranks, flavor_ranks
+
+def image_to_prompt(image, mode):
+    ci.config.chunk_size = 2048 if ci.config.clip_model_name == "ViT-L-14/openai" else 1024
+    ci.config.flavor_intermediate_count = 2048 if ci.config.clip_model_name == "ViT-L-14/openai" else 1024
+    image = image.convert('RGB')
+    if mode == 'best':
+        return ci.interrogate(image)
+    elif mode == 'classic':
+        return ci.interrogate_classic(image)
+    elif mode == 'fast':
+        return ci.interrogate_fast(image)
+    elif mode == 'negative':
+        return ci.interrogate_negative(image)
+
+def prompt_tab():
+    with gr.Column():
+        with gr.Row():
+            image = gr.Image(type='pil', label="Image")
+            with gr.Column():
+                mode = gr.Radio(['best', 'fast', 'classic', 'negative'], label='Mode', value='best')
+        prompt = gr.Textbox(label="Prompt")
+    button = gr.Button("Generate prompt")
+    button.click(image_to_prompt, inputs=[image, mode], outputs=prompt)
+
+def analyze_tab():
+    with gr.Column():
+        with gr.Row():
+            image = gr.Image(type='pil', label="Image")
+        with gr.Row():
+            medium = gr.Label(label="Medium", num_top_classes=5)
+            artist = gr.Label(label="Artist", num_top_classes=5)
+            movement = gr.Label(label="Movement", num_top_classes=5)
+            trending = gr.Label(label="Trending", num_top_classes=5)
+            flavor = gr.Label(label="Flavor", num_top_classes=5)
+    button = gr.Button("Analyze")
+    button.click(image_analysis, inputs=image, outputs=[medium, artist, movement, trending, flavor])
+
+with gr.Blocks() as ui:
+    with gr.Tab("Prompt"):
+        prompt_tab()
+    with gr.Tab("Analyze"):
+        analyze_tab()
+
+ui.launch(show_api=False, debug=False)
